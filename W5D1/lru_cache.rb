c868e7add91d858734524bc6e33ffdf14b3ce8ed
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-02-13 14:25:07 -0800

    Debug and test lru.cache.rb

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -1,5 +1,5 @@
 class LRUCache
-  def initialize
+  def initialize(size)
     @size = size
     @cache = []
   end
@@ -11,10 +11,10 @@ class LRUCache
 
   def add(el)
     # adds element to cache according to LRU principle
-    if @cache.inclue?(el)
+    if @cache.include?(el)
       @cache.delete(el)
       @cache << el
-    else count >= @size
+    elsif count >= @size
       @cache.shift
       @cache << el
     else
@@ -31,3 +31,23 @@ class LRUCache
   # helper methods go here!
 
 end
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+
+p johnny_cache.count # => returns 2
+
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+p johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
